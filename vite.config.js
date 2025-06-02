975574019be1b0a2db28bfcab36cab42e0f750b0
Rohit Kamble <rohitkamble6458@gmail.com>
2025-06-02 22:54:34 +0530

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1' 
-    ? '/portfolio-website/' 
-    : '/'
+  base: 'https://github.com/Rohitkamble6458/portfolio-website',
 })
